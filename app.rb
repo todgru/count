56cdb6e98d24f1dcdc5eb0d1adf251a7234f2414
todgru <todgru@gmail.com>
2015-04-11 22:26:40 -0700

    move around

--- app.rb
+++ app.rb
@@ -10,6 +10,11 @@ get '/count' do
   erb :index
 end
 
+post '/' do
+  @count = @params['item_count'].to_i
+  erb :index
+end
+
 post '/count' do
   @count = @params['item_count'].to_i
   erb :index
